deb7851c38b10d29c77197fa20ed56b8dcc52c9f
jul0706 <yu.ivan@rambler.ru>
2023-06-08 13:45:19 +0500

    check errors1

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,34 @@
 const User = require('../models/user');
 
+function checkValidationError (error, res) {
+  if (error.name === 'ValidationError') {
+    return res
+      .status(400)
+      .send({
+        message: 'Incorrect data',
+      });
+  }
+}
+
 const getUsers = (req, res) => {
   User.find({})
+    .orFail(() => new Error('Not found'))
     .then((users) => res.status(200).send(users))
-    .catch((err) => res.status(500).send({ message: 'Iternal server error', err: err.message, stack: err.stack }));
+    .catch((err) => {
+      if (err.message === 'Not found') {
+        res
+          .status(404)
+          .send({
+            message: 'User not found',
+          });
+      } else {
+        res
+          .status(500)
+          .send({
+            message: 'Iternal server error', err: err.message, stack: err.stack,
+          });
+      }
+    });
 };
 
 const getUserById = (req, res) => {
@@ -30,19 +55,76 @@ const getUserById = (req, res) => {
 const createUser = (req, res) => {
   User.create(req.body)
     .then((user) => res.status(201).send(user))
-    .catch((err) => res.status(500).send({ message: 'Iternal server error', err: err.message, stack: err.stack }));
+    .catch((err) => {
+      checkValidationError(err, res);
+       if (!req.body.name || !req.body.about || !req.body.avatar) {
+        res
+          .status(400)
+          .send({
+            message: 'Incorrect data, the request must contain: name, about, avatar',
+          });
+      } else {
+        res
+          .status(500)
+          .send({
+            message: 'Iternal server error', err: err.message, stack: err.stack,
+          });
+      }
+    });
 };
 
 const updateUserInfo = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { ...req.body }, { new: true })
+    .orFail(() => new Error('Not found'))
     .then((newUser) => res.status(201).send(newUser))
-    .catch((err) => res.status(500).send({ message: 'Iternal server error', err: err.message, stack: err.stack }));
+    .catch((err) => {
+      if (!req.body) {
+        res
+          .status(400)
+          .send({
+            message: 'Incorrect data, the request must contain: name or about or avatar',
+          });
+      } else if (err.message === 'Not found') {
+        res
+          .status(404)
+          .send({
+            message: 'User not found',
+          });
+      } else {
+        res
+          .status(500)
+          .send({
+            message: 'Iternal server error', err: err.message, stack: err.stack,
+          });
+      }
+    });
 };
 
 const updateUserAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id, ({ avatar: req.body.avatar }), { new: true })
+    .orFail(() => new Error('Not found'))
     .then((newUser) => res.status(201).send(newUser))
-    .catch((err) => res.status(500).send({ message: 'Iternal server error', err: err.message, stack: err.stack }));
+    .catch((err) => {
+      if (!req.body.avatar) {
+        res
+          .status(400)
+          .send({
+            message: 'Incorrect data, the request must contain avatar',
+          });
+      } else if (err.message === 'Not found') {
+        res
+          .status(404)
+          .send({
+            message: 'User not found',
+          });
+      } else {
+        res
+          .status(500)
+          .send({
+            message: 'Iternal server error', err: err.message, stack: err.stack,
+          });
+      }
+    });
 };
 
 module.exports = {
