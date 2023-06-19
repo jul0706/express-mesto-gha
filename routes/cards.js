70ed31791bd7863345adbaf0b16befafb91a02b8
jul0706 <yu.ivan@rambler.ru>
2023-06-19 18:51:46 +0500

    check errors14.7

--- routes/cards.js
+++ routes/cards.js
@@ -13,8 +13,8 @@ router.delete('/:cardId', deleteCard);
 
 router.post('/', celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    link: Joi.string().min(2),
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().min(2),
   }),
 }), createCard);
 
